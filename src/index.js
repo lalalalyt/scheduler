0b3f915def9ea07e375fdd237d993731307e8977
lalalalyt <lynneli318@gmail.com>
2022-09-15 21:22:44 -0400

    change order

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import Application from "components/Application";
 
 import axios from "axios";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
-
-
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
+ReactDOM.render(<Application />, document.getElementById("root"));
